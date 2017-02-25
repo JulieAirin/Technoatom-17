4ebd695d181085083e96ce24f5efbb5053b5179b
JulieAirin <arin-akira@mail.ru>
2017-02-25 22:05:44 +0300

    Simple tasks done

---
+++ simple_tasks/bin/05.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use DDP;
+
+=encoding UTF8
+=head1 SYNOPSYS
+
+Поиск количества вхождений строки в подстроку.
+
+=head1 run ($str, $substr)
+
+Функция поиска количества вхождений строки $substr в строку $str.
+Пачатает количество вхождений в виде "$count\n"
+Если вхождений нет - печатает "0\n".
+
+Примеры:
+
+run("aaaa", "aa") - печатает "2\n".
+
+run("aaaa", "a") - печатает "4\n"
+
+run("abcab", "ab") - печатает "2\n"
+
+run("ab", "c") - печатает "0\n"
+
+=cut
+
+sub run {
+    my ($str, $substr) = @_;
+    my $num = 0;
+
+    my $temp = join '', split $substr, $str;
+    $num = (length($str)-length($temp))/length($substr);
+
+    print "$num\n";
+}
+run("abcab", "ab");
+1;
