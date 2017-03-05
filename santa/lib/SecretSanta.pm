e86c4d5160f9865f4fb10d30c57634bf82c9430e
JulieAirin <arin-akira@mail.ru>
2017-03-05 13:57:10 +0000

    Secret Santa: done

---
+++ santa/lib/SecretSanta.pm
@@ -0,0 +1,39 @@
+package SecretSanta;
+
+use 5.010;
+use strict;
+use warnings;
+use DDP;
+
+sub calculate {
+	my @members = @_;
+	my @res;
+	my @list = @members;
+	my %pairs;
+	for my $i (0..$#members) {
+		if (ref($members[$i]) eq 'ARRAY') {
+			$list[$i] = $members[$i][0];
+			push @list, $members[$i][1];
+			$pairs{$i} = $#list;
+			$pairs{$#list} = $i;
+		}
+	}
+	my @num = 0..$#list;
+	my $ok = 0;
+	while (!$ok) {
+		@num = sort { int(rand(3))-1 } @num;
+		$ok = 1;
+		for my $i (0..$#num) {
+			if ( ((exists $pairs{$i}) && ($pairs{$i} == $num[$i])) || ($i == $num[ $num[$i] ]) ) {
+				$ok = 0;
+				last;
+			}
+		}
+	}
+	for my $i (0..$#num) {
+		push @res,[ $list[$i], $list[ $num[$i] ] ];
+	}
+	return @res;
+}
+
+1;
